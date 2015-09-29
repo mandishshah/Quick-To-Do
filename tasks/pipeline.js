9acc7960c31e433cf3aee26050eb10dad4e5d41b
Mandish Shah <shahmandish95@gmail.com>
2015-09-28 23:17:03 -0400

    Fixed a few more issues but the to-Do is still not displaying

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -15,7 +15,7 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'bower_components/bootstrap/dist/css/bootstrap.css',
+  '/bower_components/bootstrap/dist/css/bootstrap.css',
   'styles/**/*.css'
 ];
 
@@ -31,8 +31,8 @@ var jsFilesToInject = [
   '/bower_components/jquery/dist/jquery.js',
   '/bower_components/angular/angular.js',
   '/bower_components/angular-route/angular-route.js',
-  '/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-  '/bower_components/bootstrap/dist/js/boostrap.js',
+  '/bower_components/angular-bootstrap/ui-bootstrap.js',
+  '/bower_components/bootstrap/dist/js/bootstrap.js',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
